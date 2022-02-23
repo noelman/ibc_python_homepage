74d5cb888b28c1410f74ccf34dcd2ae3d1ab3481
theNoelman <yungingn@oregonstate.edu>
2022-02-22 21:42:36 -0800

    mysql connection works

--- app.py
+++ app.py
@@ -1,30 +1,24 @@
 from flask import Flask
-import pymysql
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
 app.secret_key = "Wi8zoSxqbbVCawRcDUR0"
 
-def mysqlconnect():
-    # To connect MySQL database
-    conn = pymysql.connect(
-        host='localhost',
-        user='root', 
-        password = "",
-        db='ibc',
-        )
-      
-    cur = conn.cursor()
-    cur.execute("select * from act_franchise")
-    output = cur.fetchall()
-    print(output)
-      
-    # To close the connection
-    conn.close()
+mysql = MySQL()
+
+# config mysql
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'ibc'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
 @app.route('/')
 def index():
+    
     return "waddup"   
 
 if __name__== '__main__':
+    #mysqlconnect()
     app.secret_key='Wi8zoSxqbbVCawRcDUR0'
     app.run(debug=True)    
