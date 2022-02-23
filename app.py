a856f84ee522ea5359514299887f3590f510024b
theNoelman <yungingn@oregonstate.edu>
2022-02-22 21:29:21 -0800

    baby steps

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
 from flask import Flask
+import pymysql
 
 app = Flask(__name__)
 app.secret_key = "Wi8zoSxqbbVCawRcDUR0"
 
+def mysqlconnect():
+    # To connect MySQL database
+    conn = pymysql.connect(
+        host='localhost',
+        user='root', 
+        password = "",
+        db='ibc',
+        )
+      
+    cur = conn.cursor()
+    cur.execute("select * from act_franchise")
+    output = cur.fetchall()
+    print(output)
+      
+    # To close the connection
+    conn.close()
+
 @app.route('/')
 def index():
-    return "waddup"
+    return "waddup"   
 
 if __name__== '__main__':
     app.secret_key='Wi8zoSxqbbVCawRcDUR0'
